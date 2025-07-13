bf2cd0db04027672d3c8bd1c0eadeb7a6b959c73
zhongkang.guan <2696040354@qq.com>
2025-07-13 19:50:35 +0800

    [feat]: add h_matrix support

--- core/stitch/src/Stitch.cpp
+++ core/stitch/src/Stitch.cpp
@@ -30,13 +30,15 @@ Stitch::Stitch(int width,int height,int cam_num): single_width(width),height(hei
             }
         }
     }
-    
+    output_width = 16251;
     //TODO：已经计算出了裁剪区域，应该可以直接得到输出图像的大小
-
+    cudaMalloc(&d_h_matrices, sizeof(float) * 9 * cam_num);
     cudaMalloc(&d_inputs_y, sizeof(uint8_t*) * cam_num);
     cudaMalloc(&d_inputs_uv, sizeof(uint8_t*) * cam_num);
     cudaMalloc(&d_crop, cam_num * 4 * sizeof(int));
     cudaMemcpy(d_crop, crop, cam_num * sizeof(int), cudaMemcpyHostToDevice);
+    cudaMalloc(&d_input_linesize_y, cam_num * sizeof(int));
+    cudaMalloc(&d_input_linesize_uv, cam_num * sizeof(int));
     // 创建 HW frame context
     hw_frames_ctx = av_hwframe_ctx_alloc(cuda_handle_init::GetGPUDeviceHandle());
     AVHWFramesContext* frames_ctx = (AVHWFramesContext*)hw_frames_ctx->data;
@@ -58,9 +60,21 @@ Stitch::~Stitch() {
     cudaFree(d_inputs_y);
     cudaFree(d_inputs_uv);
     cudaFree(d_crop);
+    cudaFree(d_input_linesize_y);
+    cudaFree(d_input_linesize_uv);
+    cudaFree(d_h_matrices);
 }
 
 AVFrame* Stitch::do_stitch(AVFrame** inputs) {
+
+    static float h_matrices[5][9] = {
+        // 相机1到相机1的变换（单位矩阵）
+        {1.0f, 0.0f, 0.0f, 0.0f, 1.0f, 0.0f, 0.0f, 0.0f, 1.0f},
+        {9.99904053e-01,-4.19696324e-02,-3.38373637e+03,-6.66393412e-03,9.90487514e-01,8.77086171e+00,-3.82220590e-06,-3.81567906e-06,1.01298991e+00},
+        {1.01286684e+00,-4.95562446e-02,-6.88883065e+03,-2.96245676e-02,9.81192353e-01,7.96030310e+01,-6.24623565e-06,-8.57136710e-06,1.03008289e+00},
+        {1.02302896e+00,-5.09373902e-02,-1.03834207e+04,-4.23372572e-02,9.59948821e-01,3.90253967e+02,-1.52826843e-05,-1.50554885e-05,1.12208562e+00},
+        {1.06537166e+00,-3.55742112e-02,-1.41281088e+04,-4.50705833e-02,9.45190099e-01,4.48848430e+02,-2.93221140e-05,-1.86384708e-05,1.30890439e+00}
+    };
 
     uint8_t* gpu_inputs_y[cam_num];
     uint8_t* gpu_inputs_uv[cam_num];
@@ -87,6 +101,9 @@ AVFrame* Stitch::do_stitch(AVFrame** inputs) {
         throw std::runtime_error("Failed to allocate GPU AVFrame buffer");
     }
 
+    cudaMemcpy(d_h_matrices, h_matrices, sizeof(float) * 9 * cam_num, cudaMemcpyHostToDevice);
+
+
     cudaMemcpy(d_inputs_y, gpu_inputs_y, sizeof(uint8_t*) * cam_num, cudaMemcpyHostToDevice);
     cudaMemcpy(d_inputs_uv, gpu_inputs_uv, sizeof(uint8_t*) * cam_num, cudaMemcpyHostToDevice);
 
@@ -104,18 +121,25 @@ AVFrame* Stitch::do_stitch(AVFrame** inputs) {
         h_input_linesize_y[i] = inputs[i]->linesize[0];
     }
 
-    int* d_input_linesize_y;
-    int* d_input_linesize_uv;
-    cudaMalloc(&d_input_linesize_y, cam_num * sizeof(int));
-    cudaMalloc(&d_input_linesize_uv, cam_num * sizeof(int));
     cudaMemcpy(d_input_linesize_y, h_input_linesize_y, cam_num * sizeof(int), cudaMemcpyHostToDevice);
     cudaMemcpy(d_input_linesize_uv, h_input_linesize_uv, cam_num * sizeof(int), cudaMemcpyHostToDevice);
 
+    #define H_M
+
+    #ifdef H_M
+    launch_stitch_kernel_with_h_matrix(d_inputs_y, d_inputs_uv,
+        d_input_linesize_y, d_input_linesize_uv,
+        d_h_matrices,
+        output_y, output_uv,
+        output->linesize[0], output->linesize[1],
+        cam_num, single_width, output_width, height,stream);
+    #else
     launch_stitch_kernel_with_crop(d_inputs_y, d_inputs_uv,
                         d_input_linesize_y, d_input_linesize_uv,
                         output_y, output_uv,
                         output->linesize[0], output->linesize[1],
                         cam_num, single_width, output_width, height,
                         stream,d_crop);
+    #endif
     return output;
 }
