a9029f5be9d9f8a91fef10d1a622db1abb18f130
zhongkang.guan <2696040354@qq.com>
2025-11-18 21:12:01 +0800

    [feat]: del mediamtx launch func

--- camera_manager/src/RtspConsumer.cpp
+++ camera_manager/src/RtspConsumer.cpp
@@ -46,68 +46,6 @@ void RtspConsumer::run() {
     packet_input.clear();
 }
 
-bool RtspConsumer::init_mediamtx() {
-    auto getExecutableDir = [&]() -> std::string {
-        char buf[PATH_MAX];
-        ssize_t len = readlink("/proc/self/exe", buf, sizeof(buf)-1);
-        if (len == -1) {
-            LOG_ERROR("读取程序路径失败: {}",strerror(errno));
-            return "";
-        }
-        buf[len] = '\0';
-        std::string exePath(buf);
-        size_t lastSlash = exePath.find_last_of('/');
-        return (lastSlash != std::string::npos) ? exePath.substr(0, lastSlash) : "";
-    };
-    std::string exeDir = getExecutableDir();
-    if (exeDir.empty()) {
-        LOG_ERROR("错误：无法获取程序路径！");
-        return EXIT_FAILURE;
-    }
-    std::string childPath = exeDir + "/mediamtx/mediamtx";
-    signal(SIGCHLD, SIG_IGN);
-    pid = fork();
-    if (pid == -1) {
-        LOG_ERROR("fork失败: {}",strerror(errno));
-        return EXIT_FAILURE;
-    } else if (pid == 0) {
-        execl(childPath.c_str(), "mediamtx", nullptr);
-        LOG_ERROR("执行失败: {}",strerror(errno));
-        _exit(EXIT_FAILURE);
-    }
-    LOG_INFO("成功启动子进程 PID: {}",pid);
-    return EXIT_SUCCESS;
-}
-
-bool RtspConsumer::destory_mediamtx() {
-    int pid = 0;
-    std::string cmd = "pgrep mediamtx";
-    FILE* pipe = popen(cmd.c_str(), "r");
-    if (!pipe) {
-        LOG_ERROR("Failed to run pgrep command");
-        return false;
-    }
-
-    char buffer[128];
-    while (fgets(buffer, sizeof(buffer), pipe)) {
-        int tmp_pid = std::atoi(buffer);
-        if(pid > 0) break;
-        if (tmp_pid > 0) {
-            pid = tmp_pid;
-        }
-    }
-    pclose(pipe);
-
-    if (kill(pid, SIGTERM) == 0) {
-        LOG_INFO("Successfully sent SIGTERM to PID {}",pid);
-    } else {
-        LOG_ERROR("Failed to kill PID {}", pid);
-    }
-    return true;
-}
-
 RtspConsumer::~RtspConsumer() {
     LOG_DEBUG("{} exit!",__func__);
 }
-
-pid_t RtspConsumer::pid = 0;
