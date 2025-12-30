6a9dcf9d81c0ab6b6e011dda7f65574b3f08bb6a
cfz2024 <2370608702@qq.com>
2025-12-30 14:51:04 +0800

    [feat][decoder]: add h264_cuvid support

--- camera_manager/src/StitchConsumer.cpp
+++ camera_manager/src/StitchConsumer.cpp
@@ -34,38 +34,31 @@ void StitchConsumer::stop() {
     }
     TaskManager::stop();
 }
-
+#define KERNEL_TEST
+#ifdef KERNEL_TEST
 void StitchConsumer::run() { 
     Frame out_image;
     AVFrame** inputs = new AVFrame*[10];
-    int first_run=0;
     LOG_DEBUG("total count {} channels", m_channelsFromDecoder.size());
+    int frame_size = 0;
+    for (auto& channel : m_channelsFromDecoder) 
+    {
+        Frame tmp;
+        if(!channel->recv(tmp)) 
+        {   
+            goto cleanup;
+        }
+        inputs[frame_size] = tmp.m_data;
+        out_image.m_costTimes.image_frame_cnt[tmp.cam_id] = tmp.m_costTimes.image_frame_cnt[tmp.cam_id];
+        out_image.m_costTimes.when_get_packet[tmp.cam_id] = tmp.m_costTimes.when_get_packet[tmp.cam_id];
+        out_image.m_costTimes.when_get_decoded_frame[tmp.cam_id] = tmp.m_costTimes.when_get_decoded_frame[tmp.cam_id];
+        frame_size ++;
+    }
     while (running) {
-        int frame_size = 0;
-        if(first_run==0)
-        {
-            first_run=1;
-            for (auto& channel : m_channelsFromDecoder) 
-            {
-                static Frame tmp;
-                LOG_DEBUG("11111");
-                if(!channel->recv(tmp)) 
-                {   
-                    LOG_DEBUG("clean");
-                    goto cleanup;
-                }
-                inputs[frame_size] = tmp.m_data;
-                out_image.m_costTimes.image_frame_cnt[tmp.cam_id] = tmp.m_costTimes.image_frame_cnt[tmp.cam_id];
-                out_image.m_costTimes.when_get_packet[tmp.cam_id] = tmp.m_costTimes.when_get_packet[tmp.cam_id];
-                out_image.m_costTimes.when_get_decoded_frame[tmp.cam_id] = tmp.m_costTimes.when_get_decoded_frame[tmp.cam_id];
-                frame_size ++;
-                LOG_DEBUG("have channel {}",frame_size);
-            }
-        }
-        else
-        {
-            
-        }
+        
+        // out_image.m_costTimes.image_frame_cnt[tmp.cam_id] ++;
+        // out_image.m_costTimes.when_get_packet[tmp.cam_id] = get_now_time();
+        // out_image.m_costTimes.when_get_decoded_frame[tmp.cam_id] = tmp.m_costTimes.when_get_decoded_frame[tmp.cam_id]; 
         out_image.m_data = ops->stitch(ops->obj, inputs);
         out_image.m_data->pts = inputs[0]->pts;
         out_image.m_costTimes.when_get_stitched_frame = get_now_time();
@@ -77,14 +70,48 @@ void StitchConsumer::run() {
 cleanup:
     for(auto& channel : m_channelsFromDecoder) {
         channel->clear();
-        LOG_DEBUG("clean up");
+    }    
+    for (int i = 0; i < m_channelsFromDecoder.size(); ++i) 
+    {
+        if (inputs[i]) {
+            av_frame_free(&inputs[i]);
+        }
     }
     delete[] inputs;
-    for (int i = 0; i < m_channelsFromDecoder.size(); ++i) {
+}
+
+#else
+void StitchConsumer::run() { 
+    Frame out_image;
+    AVFrame** inputs = new AVFrame*[10];
+    LOG_DEBUG("total count {} channels", m_channelsFromDecoder.size());
+    while (running) {
+        int frame_size = 0;
+        for (auto& channel : m_channelsFromDecoder) {
+            Frame tmp;
+            if(!channel->recv(tmp)) goto cleanup;
+            inputs[frame_size] = tmp.m_data;
+            out_image.m_costTimes.image_frame_cnt[tmp.cam_id] = tmp.m_costTimes.image_frame_cnt[tmp.cam_id];
+            out_image.m_costTimes.when_get_packet[tmp.cam_id] = tmp.m_costTimes.when_get_packet[tmp.cam_id];
+            out_image.m_costTimes.when_get_decoded_frame[tmp.cam_id] = tmp.m_costTimes.when_get_decoded_frame[tmp.cam_id];
+            frame_size ++;
+        }
+        out_image.m_data = ops->stitch(ops->obj, inputs);
+        out_image.m_data->pts = inputs[0]->pts;
+        out_image.m_costTimes.when_get_stitched_frame = get_now_time();
+        m_channel2show->send(out_image);
+        m_status.frame_cnt ++;
+        m_status.timestamp = get_now_time();
+        for (int i = 0; i < m_channelsFromDecoder.size(); ++i) {
             if (inputs[i]) {
                 av_frame_free(&inputs[i]);
-                LOG_DEBUG("have inputs");
             }
         }
-    LOG_DEBUG("stitch over");
+    }
+cleanup:
+    for(auto& channel : m_channelsFromDecoder) {
+        channel->clear();
+    }
+    delete[] inputs;
 }
+#endif
